52cd3986f2f976b4ecc5973acc0e720f9dd2ef79
CarlosPorras1617 <porrassainz17@gmail.com>
2022-10-13 19:05:48 -0700

    button navigation

---
+++ app/src/main/java/com/carlos/primeraapp/ButtonNavigation/Home.java
@@ -0,0 +1,38 @@
+package com.carlos.primeraapp.ButtonNavigation;
+
+import android.os.Bundle;
+
+import com.carlos.primeraapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.carlos.primeraapp.databinding.ActivityHomeBinding;
+
+public class Home extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_inicio, R.id.navigation_contador, R.id.navigation_config, R.id.navigation_map)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_home);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
